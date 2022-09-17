0f697afaff222e9eab3e9a872491a46fedab261a
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2022-09-17 13:16:06 -0700

    Initial Commit

---
+++ iOS-Uni-Instagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-Uni-Instagram
+//
+//  Created by Jaime Mendoza on 9/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-Uni-Instagram/iOS_Uni_InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Uni_InstagramApp.swift
+//  iOS-Uni-Instagram
+//
+//  Created by Jaime Mendoza on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Uni_InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
