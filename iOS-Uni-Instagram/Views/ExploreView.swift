82e1ab7b2ec607c097f291e8b0d4940e95c81ec8
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2022-09-30 06:29:37 -0700

    Implement LazyVGrid of PostViews

--- iOS-Uni-Instagram/Views/ExploreView.swift
+++ iOS-Uni-Instagram/Views/ExploreView.swift
@@ -10,20 +10,38 @@ import SwiftUI
 struct ExploreView: View {
     @State var text = ""
     
+    var columns: [GridItem] = [
+        GridItem(.flexible(minimum: 200), spacing: 0, alignment: .center),
+        GridItem(.flexible(minimum: 200), spacing: 0, alignment: .center)
+    ]
+    
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack {
-                    TextField("Search...", text: $text)
-                        .padding()
-                        .background(Color(.secondarySystemBackground))
-                        .cornerRadius(8)
-                        .padding()
-                        
-                    
-                    Spacer()
-                }
+            VStack {
+                TextField("Search...", text: $text)
+                    .padding(10)
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(12)
+                    .padding()
                 
+                Spacer()
+                
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(1...5, id: \.self) {num in
+                            NavigationLink(destination: PostView(userImagePath: "User\(num)", imagePath: "Image\(num)"), label: {
+                                Image("Image\(num)")
+                                    .resizable()
+                                    .frame(width: 200, height: 200)
+                                    .aspectRatio(contentMode: .fit)
+                            })
+                                
+                        }
+                    }
+
+                }
+                    
+     
             }.navigationTitle("Explore")
         }
     }
